4e4ce42883905c74f74b51abadfc0b1be1759c8e
Enel <spiritring@gmail.com>
2013-10-25 09:36:25 +0800

    增加JsonCpp
    增加JsonCpp

--- client/Simple/Template/proj.win32/main.cpp
+++ client/Simple/Template/proj.win32/main.cpp
@@ -1,17 +1,34 @@
 #include "main.h"
 #include "AppDelegate.h"
 #include "CCEGLView.h"
+#include "json/json.h"
 
 USING_NS_CC;
 
 // uncomment below line, open debug console
 // #define USE_WIN32_CONSOLE
 
+int ParseJsonFromString()
+{
+    const char* str = "{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}";
+
+    Json::Reader reader;
+    Json::Value root;
+    if (reader.parse(str, root))  // reader将Json字符串解析到root，root将包含Json里所有子元素
+    {
+        std::string upload_id = root["uploadid"].asString();  // 访问节点，upload_id = "UP000000"
+        int code = root["code"].asInt();    // 访问节点，code = 100
+    }
+    return 0;
+}
+
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                        HINSTANCE hPrevInstance,
                        LPTSTR    lpCmdLine,
                        int       nCmdShow)
 {
+    ParseJsonFromString();
+
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
